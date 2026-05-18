155f6d763d2ac3dfb8be1bdd373366b9006242d9
AsherDMckoy <ashermckoy@gmail.com>
2026-05-17 19:11:37 -0600

    HandmadeHero Day 004 animating the back buffer

--- src/main.c
+++ src/main.c
@@ -2,30 +2,40 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_keyboard.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS MAP_ANON
+#endif
 
 #define WINDOW_HEIGHT 900
 #define WINDOW_WIDTH 900
 #define global_variable static
+#define local_persist static
+#define internal static
 
-typedef struct {
-    int x;
-    int y;
-} cursor;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 global_variable bool running = true;
-/* global_variable bool functionKeyComboInitiated = false; */
-global_variable bool isClear = false;
-
-TTF_Font *font;
-TTF_TextEngine *textEngine;
-
-size_t pos;
-size_t line;
-
-char buffer[180000];
+global_variable SDL_Texture *Texture;
+global_variable void *BitMapMemory;
+global_variable int BitMapWidth;
+global_variable int BitMapHeight;
+global_variable int BytesPerPixel = 4;
 
 void *scp(void *ptr)
 {
@@ -42,22 +52,90 @@ void scc(bool code)
         exit(1);
     }
 }
-
-void fillColorBlackOrWhite(SDL_Renderer **renderer)
+internal void RenderWeirdGradient(int BlueOffset, int GreenOffset)
 {
-    if (!isClear) {
-        SDL_SetRenderDrawColor(*renderer, 0x00, 0x00, 0x00, 0x00);
+    int Width = BitMapWidth;
+    int Height = BitMapHeight;
+
+    int Pitch = Width * BytesPerPixel;
+    uint8 *Row = (uint8 *)BitMapMemory;
+    for (int Y = 0; Y < BitMapHeight; ++Y) {
+        uint32 *Pixel = (uint32 *)Row;
+        for (int X = 0; X < BitMapWidth; ++X) {
+            uint8 Blue = (X + BlueOffset);
+            uint8 Green = (Y + GreenOffset);
+
+            // ARGB8888 format: (Alpha << 24) | (Red << 16) | (Green << 8) | Blue
+            *Pixel++ = (0xFF << 24) | (Green << 8) | Blue;
+        }
+
+        Row += Pitch;
     }
-    else {
-        SDL_SetRenderDrawColor(*renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    }
-    isClear = !isClear;
 }
+internal void ResizeTexture(SDL_Renderer *Renderer, int Width, int Height)
+{
+    if (BitMapMemory) {
+        munmap(BitMapMemory, BitMapWidth * BitMapHeight * BytesPerPixel);
+    }
+    if (Texture) {
+        SDL_DestroyTexture(Texture);
+    }
+    Texture = SDL_CreateTexture(Renderer, SDL_PIXELFORMAT_ARGB8888,
+                                SDL_TEXTUREACCESS_STREAMING, Width, Height);
+    BitMapWidth = Width;
+    BitMapHeight = Height;
+    BitMapMemory = mmap(0, Width * Height * BytesPerPixel, PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+}
+internal void UpdateWindow(SDL_Window *Window, SDL_Renderer *Renderer)
+{
+    SDL_UpdateTexture(Texture, 0, BitMapMemory, BitMapWidth * BytesPerPixel);
+    SDL_RenderTexture(Renderer, Texture, NULL, NULL);
+    SDL_RenderPresent(Renderer);
+}
+
+void HandleEvent(SDL_Event *event)
+{
+    switch (event->type) {
+    case SDL_EVENT_QUIT: {
+        SDL_Log("Exiting flow state");
+        running = false;
+    } break;
+    case SDL_EVENT_WINDOW_RESIZED: {
+        SDL_Window *Window = SDL_GetWindowFromID(event->window.windowID);
+        SDL_Renderer *Renderer = SDL_GetRenderer(Window);
+        SDL_Log("SDL_WINDOW_RESIZED (%d, %d)", event->window.data1, event->window.data2);
+        ResizeTexture(Renderer, event->window.data1, event->window.data2);
+    } break;
+    case SDL_EVENT_WINDOW_FOCUS_GAINED: {
+        SDL_Log("Focused Gained!");
+    } break;
+    case SDL_EVENT_WINDOW_EXPOSED: {
+        SDL_Window *Window = SDL_GetWindowFromID(event->window.windowID);
+        SDL_Renderer *Renderer = SDL_GetRenderer(Window);
+        /* static bool IsWhite = true; */
+        /* if (IsWhite == true) { */
+        /*     SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255); */
+        /*     IsWhite = false; */
+        /* } */
+        /* else { */
+        /*     SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255); */
+        /*     IsWhite = false; */
+        /* } */
+        /* SDL_RenderClear(Renderer); */
+        /* SDL_RenderPresent(Renderer); */
+        UpdateWindow(Window, Renderer);
+    } break;
+    case SDL_EVENT_TEXT_INPUT: {
+        SDL_Log("Text input");
+    } break;
+    }
+}
+
 int main(void)
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
-    TTF_TextEngine *textEngine;
     SDL_Event event;
     /* TODO(Asher): Create a debug mode for debug loop blocks */
     /* SDL_Log("Available renderer drivers:"); */
@@ -66,65 +144,27 @@ int main(void)
     /* } */
 
     scc(SDL_InitSubSystem(SDL_INIT_VIDEO));
-    scc(TTF_Init());
 
-    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "software");
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, SDL_SOFTWARE_RENDERER);
 
     scc(SDL_CreateWindowAndRenderer("Flow Editor", WINDOW_WIDTH, WINDOW_HEIGHT,
-                                    SDL_WINDOW_BORDERLESS, &window, &renderer));
-
-    textEngine = scp(TTF_CreateRendererTextEngine(renderer));
-
-    font = (TTF_Font *)scp(TTF_OpenFont("/usr/share/fonts/TTF/PragmataPro.ttf", 18.0f));
-
-    SDL_Rect area = { .x = 0, .y = 0, .w = 100, .h = 900 };
-    TTF_Text *text;
-    scc(SDL_SetTextInputArea(window, &area, 0));
-    scc(SDL_StartTextInput(window));
-    if (SDL_TextInputActive(window)) {
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        SDL_Log("%s", SDL_GetRendererName(renderer));
-        cursor curs = { .x = 0, .y = 0 };
-        pos = 0;
-        line = 1;
-        buffer[pos] = '\0';
-        while (running) {
-            int w = 0, h = 0;
-            const float scale = 1.0f;
-
-            while (SDL_PollEvent(&event)) {
-                switch (event.type) {
-                case SDL_EVENT_QUIT: {
-                    SDL_Log("Exiting flow state");
-                    running = false;
-                } break;
-                case SDL_EVENT_TEXT_INPUT: {
-                    buffer[pos] = event.text.text[0];
-                    pos += 1;
-                    buffer[pos] = '\0';
-                } break;
-                }
-
-                /* TTF_DrawRendererText(text, dst.x, dst.y); */
-                /* SDL_RenderTexture(renderer, texture, NULL, &dst); */
-            }
-            text = scp(TTF_CreateText(textEngine, font, buffer, pos));
-            scc(TTF_SetTextColor(text, 0, 0, 255, 255));
-            SDL_RenderClear(renderer);
-            if (text) {
-                TTF_DrawRendererText(text, curs.x, curs.y);
-            }
-            SDL_RenderPresent(renderer);
+                                    SDL_WINDOW_BORDERLESS /*| SDL_WINDOW_RESIZABLE*/,
+                                    &window, &renderer));
+    ResizeTexture(renderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+    int XOffset = 0;
+    int YOffset = 0;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            HandleEvent(&event);
         }
-        TTF_DestroyText(text);
-        SDL_StopTextInput(window);
-        TTF_CloseFont(font);
-        TTF_DestroyRendererTextEngine(textEngine);
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        TTF_Quit();
-        SDL_Quit();
-
-        return 0;
+        RenderWeirdGradient(XOffset, YOffset);
+        UpdateWindow(window, renderer);
+        ++XOffset;
+        YOffset += 2;
     }
+    SDL_StopTextInput(window);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
